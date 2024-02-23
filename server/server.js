d730112c11e57502459fd8c61512f331be8a2ce7
Matt Allen <matt.w.allen@gmail.com>
2024-02-22 19:38:10 -0800

    post request templated

--- server/server.js
+++ server/server.js
@@ -1,12 +1,54 @@
-const express = require('express')
+/*
+Post = user ID & Image File
+Get = user ID
 
-const PORT = 3003
-const app = express()
+Both will return the image file
 
-app.use('/')
+https://medium.com/@edsin.delikumar/node-js-with-aws-sdk-integrating-s3-bucket-in-express-7f203c0f8c87
+*/
 
+const { S3Client } = require('@aws-sdk/client-s3');
+const express = require('express');
+const multer = require('multer');
+const { Readable } = reuire('stream');
+
+const PORT = 3003;
+const app = express();
+
+const s3Client = new S3Client({
+    region:,
+    credentials: {
+        accessKeyID:,
+        secretAccessKey:
+    },
+});
+
+const upload = multer({ dest: '/' });
+
+    // Define a route to handle file uploads
+app.post('/',upload.single('file'), async (req, res) => {
+    const fileStream = Readable.from(req.file.buffer);
+
+    const params = {
+        Bucket: 'YOUR_S3_BUCKET_NAME',
+        Key: req.file.originalname,
+        Body: fileStream,
+      };
+    
+      try {
+        await s3Client.send(new PutObjectCommand(params));
+        console.log('File uploaded successfully');
+        res.status(200).send('File uploaded');
+      } catch (err) {
+        console.error(err);
+        res.status(500).send('Failed to upload file');
+      }
+});
+
+app.get('/');
 
 app.listen(PORT, () => {
     console.log(`Server listening on port: ${PORT}`)
-})
+});
+
 module.exports = app;
