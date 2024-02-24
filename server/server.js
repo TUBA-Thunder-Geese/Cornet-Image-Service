c301a353916ab22c330c1270459c38d42c380b42
Matt Allen <matt.w.allen@gmail.com>
2024-02-24 12:21:56 -0800

    added functionality to upload images to s3 bucket. Testing needed

--- server/server.js
+++ server/server.js
@@ -5,46 +5,55 @@ Get = user ID
 Both will return the image file
 
 https://medium.com/@edsin.delikumar/node-js-with-aws-sdk-integrating-s3-bucket-in-express-7f203c0f8c87
+https://www.youtube.com/watch?v=eQAIojcArRY&t=1230s
 */
-
-const { S3Client } = require('@aws-sdk/client-s3');
 const express = require('express');
 const multer = require('multer');
-const { Readable } = reuire('stream');
+const dotenv = require('dotenv');
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 
 const PORT = 3003;
 const app = express();
 
-const s3Client = new S3Client({
-    region:,
+dotenv.config()
+
+// S3 access credentials
+const bucketName = process.env.BUCKET_NAME
+const bucketRegion= process.env.BUCKET_REGION
+const accessKey= process.env.ACCESS_KEY
+const secretAccessKey= process.env.SECRET_ACCESS_KEY
+
+
+// creating an S3Client class with access credentials
+const s3 = new S3Client({
     credentials: {
-        accessKeyID:,
-        secretAccessKey:
+      accessKeyID: accessKey,
+      secretAccessKey: secretAccessKey,
     },
-});
+    region: bucketRegion
+})
 
-const upload = multer({ dest: '/' });
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
-    // Define a route to handle file uploads
-app.post('/',upload.single('file'), async (req, res) => {
-    const fileStream = Readable.from(req.file.buffer);
+// Saves the user image to the S3 bucket
+app.post('/',upload.single('user_image'), async (req, res) => {
+    console.log('req.body: ', req.body)
+    console.log('req.file: ', req.file)
+    // req.file.buffer = actual image & what needs to be send to S3.
 
-    const params = {
-        Bucket: 'YOUR_S3_BUCKET_NAME',
+    const command = new PutObjectCommand({
+        Bucket: bucketName,
         Key: req.file.originalname,
-        Body: fileStream,
-      };
-    
-      try {
-        await s3Client.send(new PutObjectCommand(params));
-        console.log('File uploaded successfully');
-        res.status(200).send('File uploaded');
-      } catch (err) {
-        console.error(err);
-        res.status(500).send('Failed to upload file');
-      }
+        Body: req.file.buffer,
+        ContentType: req.file.mimetype,
+    })
+
+    await s3.send(command)
+
 });
 
+// returns the user image
 app.get('/');
 
 app.listen(PORT, () => {
