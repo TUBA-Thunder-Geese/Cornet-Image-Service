2836acf03e028e943599dbe12ef88ae2cd9c2b66
Matt Allen <matt.w.allen@gmail.com>
2024-02-24 14:22:40 -0800

    functionality roughed in. polish and testing needed

--- server/server.js
+++ server/server.js
@@ -10,8 +10,10 @@ https://www.youtube.com/watch?v=eQAIojcArRY&t=1230s
 const express = require('express');
 const multer = require('multer');
 const dotenv = require('dotenv');
-const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
+const { S3Client, PutObjectCommand, GetObjectCommand } = require("@aws-sdk/client-s3");
 const sharp = require('sharp');
+const { getSignedUrl } = require("@aws-sdk/s3-request-presigner");
+const db = require('./model/model')
 
 const PORT = 3003;
 const app = express();
@@ -56,12 +58,45 @@ app.post('/postImage',upload.single('user_image'), async (req, res) => {
 
     await s3.send(command)
 
-    // add entry to SQL data base with user_id, image_id, url
+    // get image url from newly created bucket entry
+    const getObjectParams = {
+        Bucket: bucketName,
+        Key: `${req.body.user_id}_profile_image`
+    }
+    const urlCommand = new GetObjectCommand(getObjectParams)
+    const imageUrl = await getSignedUrl(s3, urlCommand)
+    console.log('imageUrl: ', imageUrl)
 
+    // add entry to SQL data base with user_id, image_id, url
+    db.query('INSERT INTO images (user_id, image_url) VALUES ($1, $2)', [req.body.user_id, imageUrl])
+      .then((response) => {
+        console.log('user_id and image_url successfully added to SQL: ', response)
+        res.status(200).json({ message: 'Image added successfully!', imageUrl });
+      })
+      .catch((err) => {
+        console.log('And error ocurred when adding the user_id and image_id to SQL: ', err)
+        res.status(500).json({ error: 'An error occurred while adding the image.' });
+    })
 });
 
 // returns the user image
-app.get('/getImage');
+app.get('/getImage', async (req, res) => {
+  const { user_id } = req.body
+  
+  db.query('SELECT image_url FROM images WHERE user_id = $1', [user_id])
+  .then((response) => {
+    if (response.rows.length === 0){
+        return res.status(404).json({ error: 'no image found for ', user_id})
+    }
+    const imageUrl = response.rows[0].image_url
+    console.log('user image found: ', imageUrl)
+    res.status(200).json({ imageUrl })
+  })
+  .catch((err) => {
+    console.log('An error occurred when trying to access user image: ', err)
+    res.status(500).json({ error: 'An error occurred when trying to access user image.'})
+  })
+});
 
 app.listen(PORT, () => {
     console.log(`Server listening on image port: ${PORT}`)
